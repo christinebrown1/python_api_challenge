517da85c48d78167f377be19e2679ea166cf35d5
Christine Brown <71153920+christinebrown1@users.noreply.github.com>
2022-05-10 15:27:06 -0500

    Add files via upload

---
+++ python-api-cb-master/Instructions/starter_code/config.py
@@ -0,0 +1,7 @@
+# Enter your API keys
+
+# api_key = "AIzaSyCVT_UDiePMhBmnl4hjOSMiVYU0kUf9Q_0"
+
+gkey = "AIzaSyCaUcxWsNJETg6PYN1WEDFrzNSEt3HBTno"
+
+weather_api_key = "d7289453551854680c119fe040dffb5f"
---
+++ python-api-cb-master/README.md
@@ -0,0 +1,5 @@
+API homework located inside instructions folder.
+
+Have a great day!
+
+Christine
